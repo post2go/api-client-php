084f851b0e67ddbbee606950e1c652d9bc917411
Bolshakov Vitaly <ooayaoo@yandex.ru>
2014-12-17 00:44:13 +0300

    Added validFor (soft detect) method.

--- src/Couriers.php
+++ src/Couriers.php
@@ -3,23 +3,27 @@
 namespace Post2GoClient;
 
 use Post2GoClient\Exception\EmptyTrackingNumber;
-use Post2GoClient\Response\CourierDetect;
 
 class Couriers extends Base
 {
     /**
+     * Return couriers list.
+     *
      * @return \Post2GoClient\Response\Couriers
      */
     public function get()
     {
         $response = new Response($this->getRequest()->call('getCouriers'));
+
         return $response->couriers();
     }
 
     /**
+     * Return couriers that can use this tracking number.
+     *
      * @param $trackingNumber
-     * @return CourierDetect
-     * @throws Exception\EmptyTrackingNumber
+     * @return \Post2GoClient\Response\CourierDetect
+     * @throws \Post2GoClient\Exception\EmptyTrackingNumber
      */
     public function detect($trackingNumber)
     {
@@ -27,9 +31,31 @@ class Couriers extends Base
             throw new EmptyTrackingNumber();
         }
 
-        $response = new Response($this->getRequest()->call('detectCourier', array(
+        $response = new Response($this->getRequest()->call('detectCourier', [
             'tracking_number' => $trackingNumber
-        )));
+        ]));
+
+        return $response->courierDetect();
+    }
+
+    /**
+     * Soft detect method.
+     * Return all couriers except couriers that can't use this tracking number.
+     *
+     * @param $trackingNumber
+     * @return \Post2GoClient\Response\CourierDetect
+     * @throws \Post2GoClient\Exception\EmptyTrackingNumber
+     */
+    public function validFor($trackingNumber)
+    {
+        if (empty($trackingNumber)) {
+            throw new EmptyTrackingNumber();
+        }
+
+        $response = new Response($this->getRequest()->call('validForCourier', [
+            'tracking_number' => $trackingNumber
+        ]));
+
         return $response->courierDetect();
     }
 }
